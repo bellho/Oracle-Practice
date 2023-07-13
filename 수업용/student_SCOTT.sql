d8f48ff32cac5f520e3c9c00631f853dbceea09a
bellho <1313kjh@gmail.com>
2023-07-13 10:51:59 +0900

    DDL수업 - CREATE
    TABLE(물리적 공간)을 이용한 복사와, VIEW(논리적 공간)를 이용한 복사 방법

--- 수업용/student_SCOTT.sql
+++ 수업용/student_SCOTT.sql
@@ -1,4 +1,86 @@
 SELECT *
+FROM EMP
+WHERE MGR IS NULL 
+AND COMM IS NOT NULL
+CREATE USER SCOTT DEF
+;
+
+SELECT ENAME, DEPTNO, SAL
+FROM EMP
+WHERE DEPTNO IN (20, 30);
+
+SELECT ename, sal, job
+FROM EMP
+WHERE JOB IN ('ANALYST', 'SALESMAN') AND SAL >= 2500
+;
+-- 사원명의 길이와 byte 크기를 조회
+SELECT LENGTH(ENAME), LENGTHB(ENAME) 
+FROM EMP
+;
+
+SELECT ' a안 녕b ', LENGTH(' a안 녕b '), LENGTHB(' a안 녕b ')
+FROM DUAL
+;
+
+SELECT trim(' a안 녕b ') || '안녕', LENGTH(trim(' a안 녕b ')), LENGTHB(trim(' a안 녕b '))
+FROM DUAL
+;
+
+SELECT RTRIM(LTRIM(ENAME, 'S'), 'S') FROM EMP;
+
+SELECT CONCAT(ename, COMM) FROM emp;
+
+SELECT sal||'달러' FROM EMP;
+SELECT CONCAT(sal, '달러')
+FROM emp;
+
+SELECT replace(ename, 'SM', 'A')
+FROM emp;
+
+SELECT  ename, INSTR(ename, 'R', -1, 1)
+FROM emp;
+
+SELECT hiredate, ADD_MONTHS(hiredate, 1) FROM emp; 
+
+SELECT sysdate, TO_CHAR(sysdate, 'yyyy.mm.dd') FROM dual;
+
+SELECT sysdate, TO_CHAR(sysdate, 'yyyy.mm.dd (dy)') FROM dual;
+
+
+SELECT ename, TO_CHAR(empno,'000000') , TO_CHAR(sal, 'L999,999,999,999') 
+FROM emp;
+
+SELECT TO_NUMBER('1,000,000', '99,999,999') * 5 숫자로형변환
+FROM DUAL;
+
+--if(substr(emp_no, 8, 1) == 2){
+--	RETURN "여";
+--} ELSE if(substr(emp_no, 8, 1) == 4){
+--	RETURN "여";
+--	if(substr(emp_no, 8, 1) == 1){
+--	RETURN "남";
+--} ELSE if(substr(emp_no, 8, 1) == 3){
+--	RETURN "남";
+--} ELSE {
+--	RETURN "그외";
+--}
+--
+--switch(substr(emp_no, 8, 1)) {
+--	CASE 1;
+
+SELECT AVG(SAL) 평균급여 FROM EMP;
+
+SELECT MAX(SAL) 최고급여 FROM EMP;
+
+-- 부서별 평균 급여 조회
+SELECT AVG(SAL) 평균급여, DEPTNO 부서 FROM EMP GROUP BY DEPTNO;
+SELECT SUM(SAL) 평균급여, DEPTNO 부서 FROM EMP GROUP BY DEPTNO;
+SELECT MAX(SAL) 평균급여, DEPTNO 부서 FROM EMP GROUP BY DEPTNO;
+SELECT MIN(SAL) 평균급여, DEPTNO 부서 FROM EMP GROUP BY DEPTNO;
+SELECT COUNT(SAL) 평균급여, DEPTNO 부서 FROM EMP GROUP BY DEPTNO;
+-- ****************************************************************************************************
+
+SELECT *
 FROM SALGRADE s 
 ;
 -- 학생용 SCOTT 명령어들
@@ -221,9 +303,40 @@ SELECT * FROM USER_constraints;
 SELECT * FROM USER_tables;
 SELECT * FROM USER_views;
 
+-- 2023/07/13
+-- DDL 수업 CREATE
 
+CREATE TABLE emp_copy1 AS SELECT * FROM emp; -- 첫 번째 복사 TABLE - 물리적 공간
+SELECT * FROM emp_copy1;
 
+CREATE VIEW view_emp1 AS SELECT * FROM emp; -- 두 번째 복사 VIWE - 논리적 공간
+SELECT * FROM view_emp1;
 
+-- DESC EMP; 테이블을 확인하는 건데 디비버에선 사용이 안됨
+
+INSERT INTO emp VALUES(8000, 'EJKIM', 'KH', 7888, SYSDATE, 3000, 700, 40); -- RECODE 추가
+
+COMMIT; -- INSERT 하면 반드시 COMMIT 해주어야 함
+
+INSERT INTO emp_copy1 VALUES(8001, 'EJKIM', 'KH', 7888, SYSDATE, 3000, 700, 40); -- RECODE 추가
+
+COMMIT; -- INSERT 하면 반드시 COMMIT 해주어야 함
+
+INSERT INTO view_emp1 VALUES(8002, 'EJKIM', 'KH', 7888, SYSDATE, 3000, 700, 40); -- RECODE 추가
+
+COMMIT; -- INSERT 하면 반드시 COMMIT 해주어야 함
+
+CREATE TABLE emp_copy20 AS -- 첫 번째 복사 TABLE - 물리적 공간
+SELECT EMPNO , ENAME 사원명, JOB, HIREDATE  , SAL 
+FROM EMP
+WHERE deptno = 20
+;
+
+SELECT * FROM emp_copy20; -- 테이블 확인
+
+SELECT * FROM USER_CONSTRAINTS;
+
+-- DDL 수업 ALTER : 정의된 내용을 수정할 때 , DROP : 
 
 
 
